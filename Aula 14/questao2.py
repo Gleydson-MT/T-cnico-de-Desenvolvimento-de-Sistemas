43665750e3a0ce081f0962147cbdbdeb564dbdef
Gleydson-MT <275184323+Gleydson-MT@users.noreply.github.com>
2026-04-29 20:30:25 -0300

    atualizaçao

---
+++ Aula 14/questao2.py
@@ -0,0 +1,4 @@
+# 2. Crie um programa que pede números inteiros até que seja digitado -1. Quando o usuário digitar -1 exiba a soma de todos os números
+
+qnt_de_num = int(input("Digite o número que desejar (Digite -1 para sair): "))
+
