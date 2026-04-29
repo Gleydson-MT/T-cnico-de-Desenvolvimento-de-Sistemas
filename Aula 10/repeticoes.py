703a455211b56d93e954096db51c62dd850ccefe
Gleydson-MT <275184323+Gleydson-MT@users.noreply.github.com>
2026-04-29 20:00:48 -0300

    Atualização de codigos

---
+++ Aula 10/repeticoes.py
@@ -0,0 +1,39 @@
+# Altere o programa abaixo para, pedir o nome de uma pessoa e imprimir "Hello, {nome}" 5 vezes.
+
+# Melhore o programa perguntando quantas vezes a pessoa deseja repetir. E imprima "Hello, {nome}" de acordo com o número que a pessoa escreveu
+
+# nome = input("Digite o seu nome: ")
+# repeticoes = int(input("Digite quantas vezes deseja repetir seu nome: "))
+
+# for i in range(repeticoes):
+#     print(f"Hello, {nome}")
+
+
+
+# Faça um programa que repete 5 vezes a seguinte operação: Pede o nome da pessoa e imprime na tela "Olá, {nome}. Seja bem vindo."
+
+# for i in range(5):
+#     nome = input(f"Digite o nome {i+1}: ")
+
+#     print(f"Olá, {nome}. Seja bem vindo.")
+
+
+# Faça um programa que imprime os números de 0 a 500
+# for i in range(501):
+#     print(i)
+
+# Faça um programa que imprime a soma dos números de 0 a 500
+soma = 0
+
+for i in range(1000001):
+    soma += i
+
+print(f"A soma é: {soma}")
+
+
+# frutas = ["Maçã", "Banana", "Laranja", "Uva", "Kiwii", "Goiaba", "Damasco"]
+
+# for fruta in frutas:
+#     print(f"{fruta} é uma fruta")
+
+
---
+++ Aula 4/soma.py
@@ -0,0 +1,12 @@
+# Construa um programa que recebe dois números e imprime a soma desses números. 
+
+#Exemplo:
+#Digite o número 1: 20
+#Digite o número 2: 30
+#Resultado da Soma: 50
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+soma = num1 + num2
+
+print(f"Resultado da soma: {soma}")
+#Melhore o programa para exibir a subtração, a multiplicação e a divisão dos números
